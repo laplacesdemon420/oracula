3c7064b766ebe71d3d58685558673dc1a6b18e95
laplacesdemon420 <laplacesdemon420@proton.me>
2022-09-24 10:42:35 +0200

    interface sketch

---
+++ contracts/src/interfaces/OOInterface.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+interface OOInterface {
+    // ask question
+    // propose answer
+    // dispute answer
+}
+
+interface DVMInterface {
+    // initialize vote
+    // commit vote
+    // reveal vote
+    // aggregate votes
+}
