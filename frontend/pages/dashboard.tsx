bb9476ee6102b814b74ac2b126f1ebacd5e4c0ec
laplacesdemon420 <laplacesdemon420@proton.me>
2022-09-27 16:20:18 +0200

    dashboard ui

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,7 +1,117 @@
 import type { NextPage } from 'next';
+import styled from 'styled-components';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import Table from '../components/Table';
 
 const Dashboard: NextPage = () => {
-  return <div>ayo</div>;
+  return (
+    <Container>
+      <Inner>
+        <AccountInfo>
+          <div>
+            <ConnectButton
+              chainStatus="none"
+              accountStatus="address"
+              showBalance={true}
+            />
+          </div>
+          <div>
+            <p>Balance: 105.3 $OPTI, Active Balance: 10 $OPTI</p>
+          </div>
+        </AccountInfo>
+        <AppInfo>
+          <div>
+            <p className="header">41</p>
+            <p className="description">Open Questions</p>
+          </div>
+          <div>
+            <p className="header">22</p>
+            <p className="description">Proposals</p>
+          </div>
+          <div>
+            <p className="header">5</p>
+            <p className="description">Disputes</p>
+          </div>
+          <div>
+            <p className="header">67</p>
+            <p className="description">Finalized</p>
+          </div>
+        </AppInfo>
+        <TableContainer>
+          <h2>Questions</h2>
+          <Table />
+        </TableContainer>
+      </Inner>
+    </Container>
+  );
 };
+
+const TableContainer = styled.div`
+  width: 100%;
+  /* border: 1px solid ${({ theme }) => theme.background.tertiary}; */
+  border-radius: 10px 10px 0 0;
+  margin-top: 1rem;
+  /* padding: 1.5rem; */
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
+
+const AccountInfo = styled.div`
+  display: grid;
+  grid-template-columns: minmax(258px, 1fr) 3fr;
+  gap: 1rem;
+  div {
+    padding: 1rem;
+    border: 1px solid ${({ theme }) => theme.background.tertiary};
+    display: flex;
+    align-items: center;
+    p {
+      font-weight: 600;
+      font-size: ${({ theme }) => theme.typeScale.header6};
+    }
+  }
+`;
+
+const AppInfo = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 0.25rem;
+    padding: 1rem;
+    height: 150px;
+    border: 1px solid ${({ theme }) => theme.background.tertiary};
+
+    .header {
+      font-weight: 800;
+      font-size: ${({ theme }) => theme.typeScale.header1};
+    }
+    .description {
+      font-weight: 600;
+      font-size: ${({ theme }) => theme.typeScale.header6};
+    }
+  }
+`;
+
+const Inner = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const Container = styled.div`
+  min-height: calc(100vh - 62px);
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  margin-top: 3rem;
+`;
 
 export default Dashboard;
