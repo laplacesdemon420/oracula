119f563ad1cbf1f0a17c87eaefe5b5d21cecfd60
laplacesdemon420 <laplacesdemon420@proton.me>
2022-11-10 08:36:53 +0100

    last

--- contracts/addresses.ts
+++ contracts/addresses.ts
@@ -1,8 +1,4 @@
 export const addresses = {
-  goerli: {
-    token: '0xC9A119E1543Dd6ea5fC7ab48cF3762570caf8AA9',
-    oo: '0x34C19059583e26873D4f0Ce7867259818fCf8b75',
-  },
   bittorrent: {
     token: '0xcA3dB11bd994233b8946a3e426922EBADa8d4B55',
     oo: '0xc38cA98faF4DCb2C44a5bE5877baFF95BB888039',
